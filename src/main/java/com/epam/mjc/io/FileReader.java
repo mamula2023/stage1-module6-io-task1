941e24ab6dda15001f2b6aacfff193ce881161a5
mamula2023 <mamulashvili.2017@gmail.com>
2023-06-18 15:12:59 +0400

    hopefully

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,35 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.util.StringTokenizer;
 
 
-public class FileReader {
-
+public class FileReader{
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String Name = "";
+        String Email = "";
+        int Age = 0;
+        long Phone = 0;
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))){
+            String line;
+            while((line = reader.readLine()) != null){
+                StringTokenizer tokenizer = new StringTokenizer(line);
+                while(tokenizer.hasMoreTokens()){
+                    String word = tokenizer.nextToken();
+                    if(word.equals("Name:"))
+                        Name = tokenizer.nextToken();
+                    if(word.equals("Email:"))
+                        Email = tokenizer.nextToken();
+                    if(word.equals("Age:"))
+                        Age = Integer.parseInt(tokenizer.nextToken());
+                    if (word.equals("Phone:"))
+                        Phone = Long.parseLong(tokenizer.nextToken());
+                }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new Profile(Name, Age, Email, Phone);
     }
 }
